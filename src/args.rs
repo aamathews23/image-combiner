8c144e7b5e846dc4f28e6d85ceba6635e384da11
Aaron Mathews <aamathews23@gmail.com>
2022-09-16 15:40:56 -0700

    added the get_nth_arg function to struct constructor

--- src/args.rs
+++ src/args.rs
@@ -4,17 +4,19 @@ fn get_nth_arg(n: usize) -> String {
 
 #[derive(Debug)]
 pub struct Args {
+  // Args struct property declaration
   pub image_1: String,
   pub image_2: String,
   pub output: String
 }
 
 impl Args {
+  // Args struct constructor
   pub fn new() -> Self {
     Args {
-      image_1: String::new(),
-      image_2: String::new(),
-      output: String::new()
+      image_1: get_nth_arg(1),
+      image_2: get_nth_arg(2),
+      output: get_nth_arg(3)
     }
   }
 }
