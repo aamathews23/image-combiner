af66225c135a3fa81693c5da153bdcade3881ea7
Aaron Mathews <aamathews23@gmail.com>
2022-09-16 16:30:13 -0700

    created image resize function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod args;
 use std::{fs::File, io::BufReader};
 use args::Args;
-use image::{ImageFormat, DynamicImage, io::Reader};
+use image::{ImageFormat, DynamicImage, io::Reader, imageops::FilterType::Triangle, GenericImageView};
 
 enum ImageDataErrors {
   DifferentImageFormats,
@@ -15,12 +15,35 @@ fn main() -> Result<(), ImageDataErrors> {
   if image_format_1 != image_format_2 {
     return Err(ImageDataErrors::DifferentImageFormats);
   }
+
+  let (image_1, image_2) = standardise_size(image_1, image_2);
+
   Ok(())
 }
 
+// Finds image from path
 fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
   let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
   let image_format: ImageFormat = image_reader.format().unwrap();
   let image: DynamicImage = image_reader.decode().unwrap();
   (image, image_format)
+}
+
+// Gets the smallest image dimensions
+fn get_smallest_dimension(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
+  let pix_1 = dim_1.0 * dim_2.1;
+  let pix_2 = dim_2.0 * dim_2.1;
+  return if pix_1 < pix_2 { dim_1 } else { dim_2 };
+}
+
+// Converts both images to have the same size
+fn standardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage) {
+  let (width, height) = get_smallest_dimension(image_1.dimensions(), image_2.dimensions());
+  println!("width: {}, height: {}\n", width, height);
+  
+  if image_2.dimensions() == (width, height) {
+    (image_1.resize_exact(width, height, Triangle), image_2)
+  } else {
+    (image_1, image_2.resize_exact(width, height, Triangle))
+  }
 }
