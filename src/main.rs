427769a8fcb335ca92f630531ee6a30613e21e68
Aaron Mathews <aamathews23@gmail.com>
2022-09-16 15:50:02 -0700

    created image finder func

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 mod args;
+use std::{fs::File, io::BufReader};
 use args::Args;
+use image::{ImageFormat, DynamicImage, io::Reader};
 
 fn main() {
   let args = Args::new();
-  println!("{:?}", args);
+  let (image_1, image_format_1) = find_image_from_path(args.image_1);
+  let (image_2, image_format_2) = find_image_from_path(args.image_2);
+}
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+  let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+  let image_format: ImageFormat = image_reader.format().unwrap();
+  let image: DynamicImage = image_reader.decode().unwrap();
+  (image, image_format)
 }
